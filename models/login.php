98e94f9d45183a402c86e1adf112e5a3366adc41
jvlobo <jvlobo@users.noreply.github.com>
2014-03-19 10:49:20 +0100

    Added Login model

--- models/login.php
+++ models/login.php
@@ -1,1 +1,86 @@
+<?php
+/**
+ * Bicheame: System to store logins for websites.
+ *
+ * @author jslirola <jslirola@andalucia-devs.com>
+ * @author jvlobo <jvlobo@andalucia-devs.com>
+ * @license http://opensource.org/licenses/mit.php MIT License
+ * @link https://github.com/andalucia-devs/bicheame 
+ */
 
+require_once("../config/db.php");
+/**
+* This class implements methods to manage the login table
+*/
+
+class login{
+	private $db;
+
+	/**
+	 * Instance the connection class.
+	 */
+	public function _construct(){
+		$this->db = new db();
+	}
+
+	/**
+	 * Retrieve all records in login table.
+	 * 
+	 * @return array
+	 */
+	public function fetchAll(){
+		return $this->db->doQuery("SELECT * FROM login");
+	}
+
+	/**
+	 * Retrieve a single record with a specific id value
+	 * @param int $id The ID of the specific row in the table
+	 * @return array
+	 */
+	public function fetchById($id){
+		$id = $this->db->sanitize_var($id);
+		
+		return $this->db->doQuery("SELECT * FROM login WHERE login_id = $id");
+	}
+
+	/**
+	 * Retrieve a single or a group of record/s on a specific domain
+	 * @param int $id The ID of the specific domain
+	 * @return array
+	 */
+	public function fetchByDomainId($id){
+		$id = $this->db->sanitize_var($id);
+
+		return $this->db->doQuery("SELECT * FROM login WHERE domain_id = $id");
+	}
+
+	/**
+	 * Insert new value on table login
+	 * @param int $domain ID of the domain at domain table
+	 * @param string $user Username
+	 * @param string $pass Password
+	 * @param string $comment Optional comment about this login
+	 * @param datetime $date Date of insertion 
+	 * @param int $positive Total of positive votes
+	 * @param int $negative Total of negative votes
+	 * @return boolean
+	 */
+	public function insert($domain, $user, $pass, $comment, $date, $positive, $negative){
+		$domain = $this->db->sanitize_var($domain);
+		$positive = $this->db->sanitize_var($positive);
+		$negative = $this->db->sanitize_var($negative);
+
+		return $this->db->query("INSERT INTO login (domain_id, login_username, login_password, login_comment, login_date, votes_positive, votes_negative) VALUES ($domain, '$user', '$pass', '$comment', $date, $positive, $negative)");
+	}
+
+	/**
+	 * Delete a single record with a specific id value
+	 * @param int $id The ID of the specific row in the table
+	 * @return boolean
+	 */
+	public function deleteById($id){
+		$id = $this->db->sanitize_var($id);
+
+		return $this->db->query("DELETE FROM login WHERE login_id = $id");
+	}
+}
